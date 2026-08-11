80556c22f282353214537b8a1c35bc7a8b0877c5
jblue254 <japhethkiprono2020@gmail.com>
2026-08-11 23:40:39 +0300

    Feat: this gives the shape and display size of a bigger and smaller screen

--- app.py
+++ app.py
@@ -2,4 +2,6 @@ import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
-root.title("EmpTrack Employee Management System")
+root.title("Employee Management System")
+root.geometry("400x300")
+root.geometry("1200x700")
