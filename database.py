192275153101b6f4cc869d406fa14d5b6013a17b
jblue254 <japhethkiprono2020@gmail.com>
2026-08-11 22:54:33 +0300

    feat:testing the connection between the mongodb and my project

--- database.py
+++ database.py
@@ -1,2 +1,19 @@
 from pymongo import MongoClient
 from config import MONGO_URI, DATABASE_NAME, COLLECTION_NAME
+
+class Database:
+    def __init__(self):
+        try:
+            self.client = MongoClient(MONGO_URI)
+
+            # Test the connection
+            self.client.admin.command("ping")
+
+            self.db = self.client[DATABASE_NAME]
+            self.collection = self.db[COLLECTION_NAME]
+
+            print("MongoDB Connected Successfully")
+
+        except Exception as error:
+            print("Connection Failed")
+            print(error)
