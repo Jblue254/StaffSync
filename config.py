d6d8f69798ad355be0ea04e9a5660699e480d32d
jblue254 <japhethkiprono2020@gmail.com>
2026-08-11 22:48:16 +0300

    feat:connecting the mongodb with my visual studio

---
+++ config.py
@@ -0,0 +1,4 @@
+MONGO_URI = "mongodb+srv://japhethkiprono2017_db_user:KwNdmlbZsG1Lf8uX@cluster0.lbjaacm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+DATABASE_NAME = "emptrack_db"
+COLLECTION_NAME = "employees"
