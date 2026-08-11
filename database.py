0ec68998a7ae0b7d10c6738cbea312746aa02768
jblue254 <japhethkiprono2020@gmail.com>
2026-08-12 00:41:27 +0300

    feat : added a CRUD process to the database

--- database.py
+++ database.py
@@ -3,17 +3,33 @@ from config import MONGO_URI, DATABASE_NAME, COLLECTION_NAME
 
 
 class Database:
+
     def __init__(self):
-        try:
-            self.client = MongoClient(MONGO_URI)
+        self.client = MongoClient(MONGO_URI)
 
-            # Test connection
-            self.client.admin.command("ping")
+        self.db = self.client[DATABASE_NAME]
 
-            self.db = self.client[DATABASE_NAME]
-            self.collection = self.db[COLLECTION_NAME]
+        self.collection = self.db[COLLECTION_NAME]
 
-            print("MongoDB Connected Successfully")
+        print("MongoDB Connected")
 
-        except Exception as e:
-            print("Database Error:", e)
+
+    def add_employee(self, employee_data):
+        return self.collection.insert_one(employee_data)
+
+
+    def get_all_employees(self):
+        return list(self.collection.find())
+
+
+    def delete_employee(self, employee_id):
+        return self.collection.delete_one(
+            {"employee_id": employee_id}
+        )
+
+
+    def update_employee(self, employee_id, updated_data):
+        return self.collection.update_one(
+            {"employee_id": employee_id},
+            {"$set": updated_data}
+        )
