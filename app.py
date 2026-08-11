fd2d743be59bb50d68955aa56439d0a8d922ae00
jblue254 <japhethkiprono2020@gmail.com>
2026-08-11 23:44:22 +0300

    feat:removed the smaller screen size of the part

--- app.py
+++ app.py
@@ -2,7 +2,15 @@ import tkinter as tk
 from tkinter import ttk
 
 root = tk.Tk()
+
 root.title("Employee Management System")
-root.geometry("400x300")
+
 root.geometry("1200x700")
+
 root.resizable(False, False)
+
+
+
+
+
+
