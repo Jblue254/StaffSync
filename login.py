0ba12bbc7b28e6e3b8efcdb5d73b61d151e5c5f5
jblue254 <japhethkiprono2020@gmail.com>
2026-08-13 23:51:28 +0300

    feat: updated the login page to allow user page redirection

--- login.py
+++ login.py
@@ -4,28 +4,47 @@ from tkinter import messagebox
 from database import login_user
 
 
+def open_dashboard(user):
+
+    role = user["role"]
+
+    root.destroy()
+
+    if role == "admin":
+
+        import admin_dashboard
+
+    elif role == "employee":
+
+        import employee_dashboard
+
+        employee_dashboard.start_dashboard(
+            user["employee_id"]
+        )
+
+
 def login():
 
     username = username_entry.get().strip()
     password = password_entry.get().strip()
 
     if not username or not password:
-        messagebox.showwarning("Missing Information", "Please enter username and password.")
+        
+        messagebox.showwarning("Missing Information","Please enter username and password.")
+
         return
 
-    user = login_user(username, password)
+    user = login_user(username,password)
 
     if user is None:
-
-        messagebox.showerror("Login Failed","Invalid username or password.")
-
+        messagebox.showerror(
+            "Login Failed",
+            "Invalid username or password."
+        )
         return
 
-    role = user["role"]
+    open_dashboard(user)
 
-    messagebox.showinfo("Login Successful",f"Welcome {username}!\nRole: {role}")
-
-    root.destroy()
 
 # LOGIN WINDOW
 
@@ -36,33 +55,26 @@ root.geometry("400x300")
 root.resizable(False, False)
 
 
-# Title
+# TITLE
 
-tk.Label(root,text="LOGIN",font=("Arial", 22, "bold")).pack(pady=20)
+tk.Label(root,text="STAFFSYNC",font=("Arial", 22, "bold")).pack(pady=20)
 
-
-# Username
+# USERNAME
 
 tk.Label(root,text="Username").pack()
-
 username_entry = tk.Entry(root,width=30)
-
 username_entry.pack(pady=5)
 
-
-# Password
+# PASSWORD
 
 tk.Label(root,text="Password").pack()
-
 password_entry = tk.Entry(root,width=30,show="*")
-
 password_entry.pack(pady=5)
 
-# Login button
+# LOGIN BUTTON
 
-login_button = tk.Button(root,text="Login",width=20,command=login)
+tk.Button(root,text="Login",width=20,command=login).pack(pady=20)
 
-login_button.pack(pady=20)
-
+# RUN
 
 root.mainloop()
